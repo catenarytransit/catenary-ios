3fb1b86b8dd4574e96adfea4847a70742051489b
snagglegrolop <75964752+snagglegrolop@users.noreply.github.com>
2025-11-14 20:37:49 -0800

    Initial Commit

---
+++ catenary-ios/catenary_iosApp.swift
@@ -0,0 +1,16 @@
+//
+//  catenary_iosApp.swift
+//  catenary-ios
+//
+//
+
+import SwiftUI
+
+@main
+struct catenary_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
