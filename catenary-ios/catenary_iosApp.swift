c95b1e20f081b6556cd6dd76003c6515267545a1
Charlie <meow@charliecat.space>
2026-09-15 19:42:49 +0200

    Add firebase base

--- catenary-ios/catenary_iosApp.swift
+++ catenary-ios/catenary_iosApp.swift
@@ -8,6 +8,7 @@ import MapLibre
 import MapLibreSwiftDSL
 import MapLibreSwiftUI
 import SwiftUI
+import Firebase
 
 
 var GlobalViewObject: viewObject = viewObject()
@@ -17,6 +18,14 @@ struct CatenaryMapsApp: App {
     @StateObject var viewobject = GlobalViewObject
     @StateObject private var searchViewModel = SearchViewModel()
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        FirebaseApp.configure()        
+        #if DEBUG
+        let providerFactory = AppCheckDebugProviderFactory()
+        AppCheck.setAppCheckProviderFactory(providerFactory)
+        #endif
+    }
         
     var body: some Scene {
         WindowGroup {
